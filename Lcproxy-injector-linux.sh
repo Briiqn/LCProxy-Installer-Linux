39bf2cab157b382c957bb0d703e028037c5093ce
Briiqn <82249811+Briiqn@users.noreply.github.com>
2021-12-06 15:45:50 -0500

    Add files via upload
    loads the server certificate into java while lunar loads

---
+++ Lcproxy-injector-linux.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#! /bin/bash
+if pgrep -x java >/dev/null   ; then
+cd ~/.lunarclient/jre/zulu*/lib/security
+keytool -keystore cacerts -trustcacerts -importcert -noprompt -alias lcproxy -storepass changeit -file ~/Downloads/server.cer
+clear
+echo injected
+sleep 1;
+clear
+exec bash "$0" "$@"
+else
+clear
+echo Waiting for java ...
+sleep 1;
+exec bash "$0" "$@"
+fi
+
+
+
+
