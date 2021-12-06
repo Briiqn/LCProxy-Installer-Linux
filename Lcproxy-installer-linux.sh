f02d7e16774857e731ba66b7934155d4afef53e1
Briiqn <82249811+Briiqn@users.noreply.github.com>
2021-12-06 16:12:34 -0500

    Added stuff

--- Lcproxy-installer-linux.sh
+++ Lcproxy-installer-linux.sh
@@ -14,14 +14,36 @@ clear
 echo _____________________________________
 echo TRUST THE CERTIFICATE BY TYPING YES
 echo _____________________________________
+#test
 keytool -keystore cacerts -trustcacerts -importcert -alias lcproxy -storepass changeit -file ~/Downloads/server.cer
+echo server certificate test complete
+mkdir ~/LCP
 wget https://raw.githubusercontent.com/Briiqn/LCProxy-Installer-Linux/main/hosts && mv hosts ~/Downloads
+wget https://raw.githubusercontent.com/Briiqn/LCProxy-Installer-Linux/main/Lcproxy-injector-linux.sh && mv Lcproxy-injector-linux.sh ~/LCP
+wget https://raw.githubusercontent.com/Briiqn/LCProxy-Installer-Linux/main/Lcproxy-injector-linux.sh.desktop && mv Lcproxy-injector-linux.sh.desktop ~/.config/autostart
 mkdir ~/host-backup
 sudo cp /etc/hosts ~/host-backup
 echo Created hosts backup @ "/home/*/host-backup"
 sudo cp ~/Downloads/hosts /etc/
 clear
+rm ~/Downloads/hosts
 echo done! Rebooting is recommended
+echo You will find the injector in "/home/$USER/LCP"
+echo autostarting injector in 5 seconds
+sleep 1;
+echo autostarting injector in 4 seconds
+sleep 1;
+echo autostarting injector in 3 seconds
+sleep 1;
+echo autostarting injector in 2 seconds
+sleep 1;
+echo autostarting injector in 1 seconds
+sleep 1;
+clear
+echo starting
+cd ~/
+chmod ugo+rwx ~/LCP/Lcproxy-injector-linux.sh
+./LCP/Lcproxy-injector-linux.sh
 #cool song
 sleep 5;
 kill -9 $PPID
