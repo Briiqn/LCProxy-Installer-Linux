e12a10a479788eec8fbbaff8da36851e4c4d6773
Briiqn <82249811+Briiqn@users.noreply.github.com>
2021-12-12 09:18:08 -0500

    Update Lcproxy-injector-linux.sh

--- Lcproxy-injector-linux.sh
+++ Lcproxy-injector-linux.sh
@@ -2,7 +2,7 @@
 #! /bin/bash
 if pgrep -x java >/dev/null   ; then
 cd ~/.lunarclient/jre/zulu*/lib/security
-keytool -keystore cacerts -trustcacerts -importcert -noprompt -alias lcproxy -storepass changeit -file ~/Downloads/server.cer
+keytool -keystore cacerts -trustcacerts -importcert -noprompt -alias lcproxy -storepass changeit -file ~/LCP/server.cer
 clear
 echo injected
 sleep 1;
@@ -10,7 +10,7 @@ clear
 exec bash "$0" "$@"
 else
 clear
-echo Waiting for java ... "(run lunar)"
+echo Waiting for java ... "(run lunar, it may reinstall the JRE but this is NORMAL, just wait until it installs and when it opens everything should be there)"
 sleep 1;
 exec bash "$0" "$@"
 fi
